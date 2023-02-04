10a5747abcaf6e8e7faa5bf9c866ce8b2d52fdb6
louih0 <araujoenriquem@gmail.com>
2023-02-04 18:19:04 -0300

    falta apenas consertar estimado

--- r2a/r2afuzzy.py
+++ r2a/r2afuzzy.py
@@ -3,17 +3,25 @@ from player.parser import *
 from r2a.ir2a import IR2A
 from player.player import *
 from math import sqrt
+from statistics import mean
+import bisect
 
 class R2AFuzzy(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
-
-        self.vazao = []
+        self.throughputs = []
+        self.request_time = 0
         self.qi = []
         self.buffers = []
+        self.resolutions = []
+        self.T = 35
+        self.TWO_THIRDS_T = (2 * self.T) /3
+        self.FOUR_T = 4 * self.T
+
 
     def handle_xml_request(self, msg):
+        self.request_time = time.perf_counter()
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
@@ -24,9 +32,52 @@ class R2AFuzzy(IR2A):
 
     def handle_segment_size_request(self, msg):
         self.buffer = self.whiteboard.get_playback_buffer_size()
-        if len(self.buffer)>1:
-            print(f"!!!!!!!!!!!!!!!!!!!!!!!!!{self.output_controller(self.buffer)}")
-        msg.add_quality_id(self.qi[0])
+        self.request_time = time.perf_counter()
+
+        next_resolution = self.qi[0]
+
+        print(len(self.buffer))
+
+        if len(self.buffer) > 0: 
+            f = self.output_controller(self.buffer)
+            avarage_thoughputs = mean(self.throughputs[-5:])
+            next_bit_rate = f * avarage_thoughputs
+            index = bisect.bisect(self.qi, next_bit_rate)
+            next_resolution = self.qi[index - 1]
+            current_resolution = self.resolutions[-1]            
+            avarage_resolutions = mean(self.resolutions[-10:])
+            estimated_buffer = 'nao interessa'
+
+            print(f'+++++++++++++ self.resolutions[-5:],mean(self.resolutions[-5:]), next_resolution, estimated_buffer')
+            print(f'+++++++++++++ {self.resolutions[-5:],mean(self.resolutions[-5:]), next_resolution, estimated_buffer}')
+
+            # if (next_resolution > current_resolution and estimated_buffer < self.T) or \
+            #     (next_resolution < current_resolution and estimated_buffer > self.T):
+            #     print("NÃO MUDOU "*5)
+            #     next_resolution = current_resolution
+
+            if next_resolution > current_resolution:
+                estimated_buffer  = self.buffer_time + (avarage_thoughputs / next_resolution -1) * 60
+                if estimated_buffer < self.T:
+                    next_resolution = current_resolution
+                    print(f'!!!!!!!!!!!! OLHA O BUFFER: {estimated_buffer}')
+            elif next_resolution < current_resolution:
+                estimated_buffer  = self.buffer_time + (avarage_thoughputs / next_resolution -1) * 60
+                print(f'!!!!!!!!!!!! OLHA O BUFFER: {estimated_buffer}')
+                if estimated_buffer > self.T:
+                    estimated_buffer = self.buffer_time + (avarage_resolutions / current_resolution - 1) * 60
+                    print(f'!!!!!!!!!!!! OLHA O BUFFER: {estimated_buffer}')
+                    if estimated_buffer > self.T:
+                        next_resolution = current_resolution 
+                        print(f'!!!!!!!!!!!! Caso 2')
+
+            
+
+            print(f'>>>>>>>>>>>>> f, self.buffer_time, current_throughput, avarage_thoughputs, current_resolution, next_bit_rate, index, next_resolution')
+            print(f'>>>>>>>>>>>>>{f, self.buffer_time, self.throughputs[-1], avarage_thoughputs, current_resolution, next_bit_rate, index, next_resolution}')
+
+        msg.add_quality_id(next_resolution)
+        self.resolutions.append(next_resolution)
         self.send_down(msg)
 
     def linear_function(self,x0,x1, diff = False):
@@ -37,9 +88,7 @@ class R2AFuzzy(IR2A):
         return a * self.buffer_time + b
 
     def output_controller(self, buffer):
-        T = 35
-        TWO_THIRDS_T = (2 * T) /3
-        FOUR_T = 4 * T
+        
         self.buffer_time = buffer[-1][1]
         try:
             previous_buffer_time = self.buffers[-1]
@@ -49,53 +98,56 @@ class R2AFuzzy(IR2A):
         FACTORS = {'N2':0.25,'N1':0.5, 'Z':1, 'P1':1.5, 'P2':2}
         short, close, long, falling, steady, rising = 0,0,0,0,0,0
 
-        if(self.buffer_time <= TWO_THIRDS_T):
+        if(self.buffer_time <= self.TWO_THIRDS_T):
             short = 1
-        elif(self.buffer_time >= T):
+        elif(self.buffer_time >= self.T):
             short = 0
         else:
-            short = self.linear_function(T, TWO_THIRDS_T)
+            short = self.linear_function(self.T, self.TWO_THIRDS_T)
         
-        if(self.buffer_time <= TWO_THIRDS_T or self.buffer_time >= FOUR_T):
+        if(self.buffer_time <= self.TWO_THIRDS_T or self.buffer_time >= self.FOUR_T):
             close = 0
-        elif(self.buffer_time <= T):
-            close = self.linear_function(TWO_THIRDS_T, T)
+        elif(self.buffer_time <= self.T):
+            close = self.linear_function(self.TWO_THIRDS_T, self.T)
         else:
-            close = self.linear_function(FOUR_T, T)
+            close = self.linear_function(self.FOUR_T, self.T)
 
-        if(self.buffer_time <= T):
+        if(self.buffer_time <= self.T):
             long = 0
-        elif(self.buffer_time >= 4*T):
+        elif(self.buffer_time >= 4*self.T):
             long = 1
         else:
-            long = self.linear_function(T, FOUR_T)
+            long = self.linear_function(self.T, self.FOUR_T)
 
         self.diff = self.buffer_time - previous_buffer_time
 
-        if self.diff <= -(TWO_THIRDS_T):
+        if self.diff <= -(self.TWO_THIRDS_T):
             falling = 1
         elif self.diff >= 0:
             falling = 0
         else:
-            falling = self.linear_function(0, - TWO_THIRDS_T, True)
+            falling = self.linear_function(0, - self.TWO_THIRDS_T, True)
 
-        if self.diff <= -(TWO_THIRDS_T) or self.diff >= FOUR_T:
+        if self.diff <= -(self.TWO_THIRDS_T) or self.diff >= self.FOUR_T:
             steady = 0
         elif self.diff <= 0:
-            steady = self.linear_function(-TWO_THIRDS_T, 0, True)
+            steady = self.linear_function(-self.TWO_THIRDS_T, 0, True)
         else:
-            steady = self.linear_function(FOUR_T, 0, True)
+            steady = self.linear_function(self.FOUR_T, 0, True)
 
         if self.diff <= 0:
             rising = 0
-        elif self.diff >= FOUR_T:
+        elif self.diff >= self.FOUR_T:
             rising = 1
         else:
-            rising = self.linear_function(0, FOUR_T, True)
+            rising = self.linear_function(0, self.FOUR_T, True)
 
+        print(f"*******************previous_buffer_time, self.buffer_time")
         print(f"*******************{previous_buffer_time, self.buffer_time}")
-        print(f'>>>>>>>>>>>>>>>>>>>>.{short, close, long}')
-        print(f'>>>>>>>>>>>>>>>>>>>>.{falling, steady, rising}')
+        print(f'*******************.short, close, long')
+        print(f'*******************.{short, close, long}')
+        print(f'*******************.falling, steady, rising')
+        print(f'*******************.{falling, steady, rising}')
         
         rules = []
 
@@ -121,6 +173,11 @@ class R2AFuzzy(IR2A):
         return f_num / f_den
 
     def handle_segment_size_response(self, msg):
+        t = time.perf_counter() - self.request_time
+        # Vazão
+        #self.vazao[self.qi] = msg.get_bit_length() / t
+        self.throughputs.append(msg.get_bit_length() / t)
+        #self.Ts[self.Tsi] = (msg.get_bit_length() / t) 
         self.send_up(msg)
 
     def initialize(self):
@@ -128,4 +185,3 @@ class R2AFuzzy(IR2A):
 
     def finalization(self):
         pass
-
